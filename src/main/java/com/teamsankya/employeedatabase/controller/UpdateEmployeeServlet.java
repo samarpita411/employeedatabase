2d2af1e79b2cd5bf48668dad18957ae2ec4e295c
Naveen Kumar R <naveensurya207@gmail.com>
2018-09-04 17:13:17 +0530

    Changes made(Data types)

--- src/main/java/com/teamsankya/employeedatabase/controller/UpdateEmployeeServlet.java
+++ src/main/java/com/teamsankya/employeedatabase/controller/UpdateEmployeeServlet.java
@@ -21,31 +21,31 @@ public class UpdateEmployeeServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     
 		EmployeeInfoBean employeeInfoBean = new EmployeeInfoBean();
-		employeeInfoBean.setId(Integer.parseInt(req.getParameter("id")));
+		employeeInfoBean.setId((req.getParameter("id")));
 		employeeInfoBean.setFirstname(req.getParameter("firstname"));
 		employeeInfoBean.setLastname(req.getParameter("lastname"));
 		
 		EmployeePersonalInfoBean employeePersonalInfoBean = new EmployeePersonalInfoBean();
-	    employeePersonalInfoBean.setId(Integer.parseInt(req.getParameter("id")));
+	    employeePersonalInfoBean.setId((req.getParameter("id")));
 	    employeePersonalInfoBean.setDateOfBirth(req.getParameter("dateOfBirth"));
 	    employeePersonalInfoBean.setPhNumber(Integer.parseInt(req.getParameter("phNumber")));
 	    employeePersonalInfoBean.setEmailId(req.getParameter("emailId"));
 	    
 	    EmployeeAddressInfoBean employeeAddressInfoBean = new EmployeeAddressInfoBean();
-	    employeeAddressInfoBean.setId(Integer.parseInt(req.getParameter("id")));
+	    employeeAddressInfoBean.setId((req.getParameter("id")));
 	    employeeAddressInfoBean.setAddress1(req.getParameter("address1"));
 	    employeeAddressInfoBean.setAddress2(req.getParameter("address2"));
 	    employeeAddressInfoBean.setCity(req.getParameter("city"));
 	    employeeAddressInfoBean.setPincode(Integer.parseInt(req.getParameter("pincode")));
 	    
         EmployeeCompanyInfoBean employeeCompanyInfoBean = new EmployeeCompanyInfoBean();
-        employeeCompanyInfoBean.setId(Integer.parseInt(req.getParameter("id")));
+        employeeCompanyInfoBean.setId((req.getParameter("id")));
         employeeCompanyInfoBean.setDateOfJoining(req.getParameter("dateOfJoining"));
         employeeCompanyInfoBean.setExperience(Integer.parseInt(req.getParameter("experience")));
         employeeCompanyInfoBean.setLastCompanyName(req.getParameter("lastCompanyName"));
         
         EmployeeDesignationInfoBean employeeDesignationInfoBean = new EmployeeDesignationInfoBean();
-        employeeDesignationInfoBean.setId(Integer.parseInt(req.getParameter("id")));
+        employeeDesignationInfoBean.setId((req.getParameter("id")));
         employeeDesignationInfoBean.setDesignation(req.getParameter("designation"));
         employeeDesignationInfoBean.setCostTocompany(Integer.parseInt(req.getParameter("costToCompany")));
 	    
