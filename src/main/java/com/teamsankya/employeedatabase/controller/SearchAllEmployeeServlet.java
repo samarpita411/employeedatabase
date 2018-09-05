d76608b2dfb35763e1edc183fdafbd1c05648802
Naveen Kumar R <naveensurya207@gmail.com>
2018-09-05 15:37:09 +0530

    SearchAllEmployeeServlet's code is done

---
+++ src/main/java/com/teamsankya/employeedatabase/controller/SearchAllEmployeeServlet.java
@@ -0,0 +1,25 @@
+package com.teamsankya.employeedatabase.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.teamsankya.employeedatabase.dao.EmployeeDAO;
+import com.teamsankya.employeedatabase.factory.EmployeeServiceManager;
+
+public class SearchAllEmployeeServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		EmployeeDAO dao= EmployeeServiceManager
+				.getInstance()
+				.daoGenerator();
+        dao.searchEmployee(name);
+        req.getRequestDispatcher("searchall_response.jsp").forward(req, resp);
+        
+	}
+
+}
